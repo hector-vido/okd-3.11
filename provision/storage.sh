e1653431e62f556e2a7664eedc71d18f30916a59
Hector Vido <hector_vido@yahoo.com.br>
2019-06-06 19:01:10 -0300

    Modified permissions for NFS directories

--- provision/storage.sh
+++ provision/storage.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 mkdir -p /volumes/v{1,2,3,4,5}
+chmod 0700 /volumes/v{1,2,3,4,5}
+chown nfsnobody: /volumes/v{1,2,3,4,5}
 
 cat > /etc/exports <<EOF
-/volumes/v1 27.11.90.0/255.255.255.0(rw,root_squash)
-/volumes/v2 27.11.90.0/255.255.255.0(rw,root_squash)
-/volumes/v3 27.11.90.0/255.255.255.0(rw,root_squash)
-/volumes/v4 27.11.90.0/255.255.255.0(rw,root_squash)
-/volumes/v5 27.11.90.0/255.255.255.0(rw,root_squash)
+/volumes/v1 27.11.90.0/255.255.255.0(rw,all_squash)
+/volumes/v2 27.11.90.0/255.255.255.0(rw,all_squash)
+/volumes/v3 27.11.90.0/255.255.255.0(rw,all_squash)
+/volumes/v4 27.11.90.0/255.255.255.0(rw,all_squash)
+/volumes/v5 27.11.90.0/255.255.255.0(rw,all_squash)
 EOF
 
 exportfs -a
-systemctl start nfs-server
-systemctl enable nfs-server
+systemctl start rpcbind nfs-server
+systemctl enable rpcbind nfs-server
